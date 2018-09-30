27453120223257da516e3c84aa00b272ab4fae22
Miguel Mercado <miggy2129@gmail.com>
2018-09-30 16:17:02 +0800

    Added Find Minimal Set Test Script Schema

---
+++ dmcp/test/test_find.py
@@ -0,0 +1,73 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from dmcp.test.base_test import BaseTest
+import numpy as np
+import cvxpy as cvx
+import dmcp.find_set as find_set
+
+class findMinimalTestCases(BaseTest):
+    def setUp(self):
+        '''
+        Used to setup all the parameters of the tests.
+        '''
+    
+    def test_findMinimalSets(self):
+        '''
+        Tests whether or not the fix variable function works for objects.
+        '''
+
+    def test_findMIS(self):
+        '''
+        Tests whether or not the fix variable function works for expressions.
+        '''
+
+    def test_findAllIset(self):
+        '''
+        Tests whether or not the fix variable function works for problems/
+        '''
+
+    def test_isIndependent(self):
+        '''
+        '''
+    
+    def test_findAllSubsets(self):
+        '''
+        '''
+    
+    def test_searchConflictL(self):
+        '''
+        '''
+
+    def test_searchConflict(self):
+        '''
+        '''
+
+    def test_isIntersect(self):
+        '''
+        '''
+
+    def test_union(self):
+        '''
+        '''
+
+    def test_findMaxsetProb(self):
+        '''
+        '''
+
+    def test_findDCPMaxset(self):
+        '''
+        '''
+
+    def test_findDCPSet(self):
+        '''
+        '''
+
+    def test_isSubset(self):
+        '''
+        '''
+
+    def test_erase(self):
+        '''
+        '''
