71e3ae62193ebf7ef219d3ec9c2cecdcf3552283
Miguel Mercado <miggy2129@gmail.com>
2018-11-17 13:38:20 +0800

    Final DMCP version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='dmcp',
-    version='0.1.3',
+    version='1.0.0',
     author='Xinyue Shen, Steven Diamond, Stephen Boyd',
     author_email='xinyues@stanford.edu, diamond@cs.stanford.edu, boyd@stanford.edu',
     packages=['dmcp'],
     license='GPLv3',
     zip_safe=False,
-    install_requires=["cvxpy >= 0.3.5"],
+    install_requires=["cvxpy >= 1.0.6"],
     use_2to3=True,
     url='http://github.com/cvxgrp/dmcp/',
     description='A CVXPY extension for multi-convex programs.',
