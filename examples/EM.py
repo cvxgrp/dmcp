6981a8867812020f6d0971d183d6e4845730934c
Miguel Mercado <miggy2129@gmail.com>
2018-12-20 15:43:44 +0800

    Defined variables

--- examples/EM.py
+++ examples/EM.py
@@ -1,21 +1,37 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+import numpy as np
 import cvxpy as cvx
 import dmcp
 
 
 # Get the iris dataset
 iris = datasets.load_iris()
+data_set = iris.data
+test_labels = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.25, random_state=42)
+# Number of classes
+num_classes = len(set(iris.target))
+num_examples = len(iris.target)
 
-print("Train")
-for i in range(len(y_train)):
-    print(X_train[i], y_train[i])
+# Define Multivariate Normal Distribution optimization variables
+mean_array = []
+precision_array = []
+for i in range(num_classes):
+    mean_array.append(cvx.Variable((4,1)))
+    precision_array.append(cvx.Variable((4,4), PSD=True))
 
-print("Test")
-for i in range(len(y_test)):
-    print(X_test[i], y_test[i])
+# Define categorical distribution optimization variables
+categorical_array = []
+for i in range(num_examples):
+    conditional_array = []
+    for j in range(num_classes):
+        conditional_array.append(cvx.Variable((1,1)))
+    categorical_array.append(conditional_array)
 
-print(len(X_train), len(X_test))
-print(len(y_train), len(y_test))
+# Create objective function
+
+
+print(mean_array)
+print(precision_array)
+print(len(categorical_array))
